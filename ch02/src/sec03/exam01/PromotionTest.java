4fa37b18ef0cf9a5ab87e623b17d55fb14b4f37c
kdongmin4954 <kdongmin4954@gmail.com>
2025-09-16 08:54:12 +0900

    0909

--- ch02/src/sec03/exam01/PromotionTest.java
+++ ch02/src/sec03/exam01/PromotionTest.java
@@ -1,10 +1,25 @@
-package sec03.exam01;
+package sec03.exam2;
+import java.util.Scanner;
 
-public class PromotionTest {
-
+public class IntTest {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		byte vall = 1;
+		byte val2 = 2;
+		
+		//byte val3 = val1 + val2;
+		
+//		System.out.println("1/2" + 1/2);
+//		System.out.println("1.0/2.0 : " + 1.0/2.0);
+		
+		String str = "10";
+		int value = 20;
+		String strl = "" + value;
+		
+		//int result = val1 + Integer.parseInt(str);
+		
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		System.out.println(input);
 	}
-
 }
