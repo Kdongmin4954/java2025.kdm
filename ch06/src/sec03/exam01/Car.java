7f7b31cb239f812cdc6e2d20ea1b631628006d0f
kdongmin4954 <kdongmin4954@gmail.com>
2025-10-02 14:45:40 +0900

    1002 실습내용

---
+++ ch06/src/sec03/exam01/Car.java
@@ -0,0 +1,41 @@
+package sec03.exam01;
+
+public class Car {
+//	String company = "Kia";
+//	String model = "Truck";
+//	int maxSpeed = 150;
+	String company;
+	String model;
+	int maxSpeed;
+	
+	public Car() {
+		this("Kia", "Truck", 150);
+	}
+	
+	public Car(String company) {
+//		this.company = company;
+		this("Kia", "Truck", 150);
+		}
+	
+	public Car(String company, String model) {
+//		this.company = company;
+//		this.model = model;
+		this("Kia", "Truck", 150);
+	}
+	
+	
+	public Car(String company, String model, int maxSpeed)
+	{
+		this.company = company;
+		this.model = model;
+		this.maxSpeed = maxSpeed;
+	}
+	
+	public void ShowInfo() {
+		System.out.println("Company :" + company);
+		System.out.println("Model :" + model);
+		System.out.println("maxSpeed :" + maxSpeed);
+	}
+	
+}
+
